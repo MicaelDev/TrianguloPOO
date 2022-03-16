779ca7c6d04ae7cb15c8ad27e5a4032519426ff7
Micael20 <74980510+MicaelDev@users.noreply.github.com>
2022-03-15 23:19:29 -0300

    Add project files.

---
+++ TrianguloPOO/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace TrianguloPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo trianguloX = new Triangulo();
+            Triangulo trianguloY = new Triangulo();
+
+            Console.WriteLine("Entre com as medidas do triângulo X: ");
+            trianguloX.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            trianguloX.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            trianguloX.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com as medidas do triângulo Y: ");
+            trianguloY.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            trianguloY.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            trianguloY.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = trianguloX.CalculaArea();
+            double areaY = trianguloY.CalculaArea();
+
+            Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Maior área: X");
+            }
+            else
+            {
+                Console.WriteLine("Maior área: Y");
+            }
+        
+        }
+    }
+}
---
+++ TrianguloPOO/Triangulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TrianguloPOO
+{
+    class Triangulo
+    {
+        //Atributos da classe
+        public double A;
+        public double B;
+        public double C;
+
+        //Método da classe
+        public double CalculaArea()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
